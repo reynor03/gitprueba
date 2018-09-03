6a52f9a00a3225567e42e91de30b3d39dd9c6a93
Raynorh <raynorh6@gmail.com>
2018-09-03 01:03:39 -0400

    Mi primer commit

---
+++ git/index1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GIT 2</title>
+</head>
+<body>
+	<h1>ESTE ES LA PRUEBA GIT</h1>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GIT</title>
+</head>
+<body>
+	<h1>ESTE ES LA PRUEBA GIT COMMIT</h1>
+</body>
+</html>
